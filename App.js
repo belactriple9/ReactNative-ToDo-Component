532f553c6ff94b99d57544baad0ea714c1247ed1
belactriple9 <carippey1@gmail.com>
2020-12-13 21:05:52 -0500

    Added ToDo core component

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import ToDoList from "./screens/toDoList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ToDoList />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/toDoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToDoHome from './toDoList/toDoHome.js'; //toDoHome.js has a stacknavigator for the other components
+
+const ToDoList = (props) => {
+
+    return (
+        <ToDoHome />
+    );
+
+}
+
+export default ToDoList;
