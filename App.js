5548e769e345e13547d2a17bbe2f95276b501bee
belactriple9 <carippey1@gmail.com>
2020-12-26 18:38:52 -0500

    changed main page

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 
 import ToDoList from "./screens/toDoList";
 
 export default function App() {
+  //AsyncStorage.clear()
   return (
     <ToDoList />
   );
